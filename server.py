d01d5c9e63b22e25c289f4c25df2653c270ba286
stillecodeur <stillecodeur@gmail.com>
2025-01-09 06:28:58 -0500

    Static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""flask module EmotionDetection module"""
 from flask import Flask,render_template,request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,22 +6,25 @@ app=Flask("Emotional Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
+    """Analyze text with emotion_detector and sends response"""
     text_to_analyze=request.args.get("textToAnalyze")
 
     response=emotion_detector(text_to_analyze)
 
     if response["dominant_emotion"] is None:
-        return "Invalid text! Please try again!"
+        result_response = "Invalid text! Please try again!"
     else:
-        return '''For the given statement, the system response is 
-                'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.
-                '''.format(response["anger"],response["disgust"],response["fear"],response["joy"],response["sadness"],response["dominant_emotion"])
-
-    
-
+        result_response = f'''For the given statement, the system response is
+                'anger': {response["anger"]}, 'disgust': {response["disgust"]},
+                'fear': { response["fear"]}, 'joy': {response["joy"]}
+                and 'sadness': {response["sadness"]}.
+                The dominant emotion is {response["dominant_emotion"]}.
+                '''
+    return result_response
 
 @app.route("/")
 def render_index_page():
+    """Render intro ui template"""
     return render_template('index.html')
 
 if __name__ == "__main__":
