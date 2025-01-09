e1ba7d348a2d17764b76ff111be3b054f814f3a0
stillecodeur <stillecodeur@gmail.com>
2025-01-09 05:53:21 -0500

    Response handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,18 +8,24 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     dataObj = { "raw_document": { "text": text_to_analyze } }
 
+    
     response = requests.post(url,json = dataObj, headers = header)
-    formatted_response=json.loads(response.text)
-
-    result_data = formatted_response["emotionPredictions"][0]["emotion"]
-
-    dominant_emotion_value = 0.0
-    dominant_emotion = ""
-    for key,value in result_data.items():
-        if(dominant_emotion_value < value):
-           dominant_emotion_value = value
-           dominant_emotion = key
+    result_data=None
+    if response.status_code==200:
+        formatted_response=json.loads(response.text)
+        result_data = formatted_response["emotionPredictions"][0]["emotion"]
+        dominant_emotion_value = 0.0
+        dominant_emotion = ""
+        for key,value in result_data.items():
+            if dominant_emotion_value < value:
+                dominant_emotion_value = value
+                dominant_emotion = key
+            
+        result_data["dominant_emotion"] = dominant_emotion
+    elif response.status_code==400:
+        result_data={"anger":None,"disgust":None,"fear":None,"joy":None,"sadness":None,"dominant_emotion":None}
         
-    result_data["dominant_emotion"] = dominant_emotion
-
     return result_data
+        
+
+  
--- server.py
+++ server.py
@@ -9,9 +9,14 @@ def emo_detector():
 
     response=emotion_detector(text_to_analyze)
 
-    return '''For the given statement, the system response is 
-    'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.
-    '''.format(response["anger"],response["disgust"],response["fear"],response["joy"],response["sadness"],response["dominant_emotion"])
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+    else:
+        return '''For the given statement, the system response is 
+                'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.
+                '''.format(response["anger"],response["disgust"],response["fear"],response["joy"],response["sadness"],response["dominant_emotion"])
+
+    
 
 
 @app.route("/")
